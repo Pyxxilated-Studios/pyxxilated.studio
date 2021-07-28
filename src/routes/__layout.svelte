2eddf168a9b84a0f2b960353354bbe1b762dd027
Josh Hill <github@pyxxilated.studio>
2021-07-28 21:21:34 +1200

    chore: remove more classes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,11 @@
 <header>
 	<nav>
-		<a href="/" class="brand"><h2>Pyxxilated Studios</h2></a>
+		<a href="/"><h2>Pyxxilated Studios</h2></a>
 
-		<ul class="brand-links">
-			<a href="/blog" class="brand-link">Blog</a>
-			<a href="/projects" class="brand-link">Projects</a>
-			<a href="/about" class="brand-link">About</a>
+		<ul>
+			<a href="/blog">Blog</a>
+			<a href="/projects">Projects</a>
+			<a href="/about">About</a>
 		</ul>
 	</nav>
 </header>
@@ -16,7 +16,7 @@
 
 <footer>
 	<a href="https://www.github.com/pyxxil">
-		<svg class="icon" aria-label="Github" xmlns="http://www.w3.org/2000/svg">
+		<svg aria-label="Github" xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
 			/>
@@ -24,7 +24,7 @@
 	</a>
 
 	<a href="https://www.gitlab.com/pyxxil">
-		<svg class="icon" aria-label="Gitlab" xmlns="http://www.w3.org/2000/svg">
+		<svg aria-label="Gitlab" xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="M22.65 14.39L12 22.13 1.35 14.39a.84.84 0 0 1-.3-.94l1.22-3.78 2.44-7.51A.42.42 0 0 1 4.82 2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44 7.49h8.1l2.44-7.51A.42.42 0 0 1 18.6 2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44 7.51L23 13.45a.84.84 0 0 1-.35.94z"
 			/>
@@ -32,7 +32,7 @@
 	</a>
 
 	<a href="https://www.linkedin.com/in/pyxxil">
-		<svg class="icon" aria-label="LinkedIn" xmlns="http://www.w3.org/2000/svg">
+		<svg aria-label="LinkedIn" xmlns="http://www.w3.org/2000/svg">
 			<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
 			<rect x="2" y="9" width="4" height="12" />
 			<circle cx="4" cy="4" r="2" />
@@ -53,7 +53,7 @@
 
 	:global body {
 		background-color: var(--background);
-		margin-top: 5em;
+		margin-top: 2em;
 		font: 16px 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial,
 			'Lucida Grande', sans-serif;
 	}
@@ -79,15 +79,11 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		flex: 1 0 auto;
 	}
 
 	header {
+		height: 3em;
 		background-color: var(--background);
-	}
-
-	header {
-		height: 5em;
 		top: 0;
 		width: 100%;
 		position: fixed;
@@ -97,14 +93,14 @@
 		align-items: center;
 		justify-content: space-between;
 		width: 100%;
-		margin-right: 1em;
 	}
 
 	nav a {
 		text-decoration: none;
+		margin: 0.5em 0.5em;
 	}
 
-	.brand,
+	nav a,
 	main {
 		color: var(--primary);
 	}
@@ -114,11 +110,7 @@
 		display: flex;
 	}
 
-	.brand h2 {
-		margin: 0.5em 0.5em;
-	}
-
-	.brand-links {
+	nav ul {
 		display: flex;
 		list-style: outside none none;
 		margin: 0 0.5em;
@@ -126,7 +118,7 @@
 		margin-block: 0;
 	}
 
-	.brand-link {
+	nav ul a {
 		margin: 0 0.25em;
 		color: var(--primary);
 	}
@@ -158,21 +150,6 @@
 		color: var(--accent) !important;
 	}
 
-	svg:hover {
-		fill: var(--accent) !important;
-		transform: scale(1.25);
-	}
-
-	.icon {
-		width: 24px;
-		height: 24px;
-		stroke: var(--primary);
-		stroke-width: 2;
-		stroke-linecap: round;
-		stroke-linejoin: round;
-		fill: none;
-	}
-
 	footer {
 		display: flex;
 		justify-content: center;
@@ -192,7 +169,19 @@
 		padding: 0.5em;
 	}
 
-	footer li svg {
+	footer svg {
 		fill: var(--primary);
+		width: 24px;
+		height: 24px;
+		stroke: var(--primary);
+		stroke-width: 2;
+		stroke-linecap: round;
+		stroke-linejoin: round;
+		fill: none;
+	}
+
+	footer svg:hover {
+		fill: var(--accent) !important;
+		transform: scale(1.25);
 	}
 </style>

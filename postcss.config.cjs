29fd6e30534924a8b9eff6b3f6f272bbfd48225d
Josh Hill <github@pyxxilated.studio>
2021-10-31 01:25:13 +1300

    feat: Optimise JS Output

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,5 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
 const purgecss = require('@fullhuman/postcss-purgecss');
 const csso = require('postcss-csso');
 
@@ -8,10 +7,6 @@ module.exports = {
 	plugins: [
 		tailwindcss,
 		autoprefixer,
-		cssnano({
-			autoprefixer: false,
-			preset: ['default']
-		}),
 		purgecss({
 			content: ['./**/*.html', './**/*.svelte'],
 			defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import static_adapater from '@sveltejs/adapter-static';
+import { uglify } from 'rollup-plugin-uglify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +11,14 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: static_adapater()
+		adapter: static_adapater(),
+		vite: {
+			build: {
+				rollupOptions: {
+					plugins: [uglify()]
+				}
+			}
+		}
 	}
 };
 

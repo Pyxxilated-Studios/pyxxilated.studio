681d40c89f77d7950981490dce5f149d8a7cd3d3
Josh Hill <github@pyxxilated.studio>
2021-10-31 23:17:46 +1300

    feat: Add Labels on projects page

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -88,11 +88,12 @@
 				{project.title}
 			</h3>
 
-			<button class="ml-auto align-middle">
+			<button class="ml-auto align-middle" aria-label="Expand Summary">
 				<svg
 					class="transition-all duration-300 fill-current opacity-75 w-4 h-4 -mr-1 stroke-foreground fill-foreground"
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 20 20"
+					aria-label="Expand Arrow"
 				>
 					<path
 						d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"
